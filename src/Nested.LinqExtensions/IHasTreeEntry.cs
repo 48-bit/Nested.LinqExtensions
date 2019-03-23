885a5f9147959c5a642f23af3bcf7677ee76d17f
benedictt <re_b_e_l@mail.ru>
2019-03-23 19:26:27 +0300

    xml-doc for IHasTreeEntry interface

--- src/Nested.LinqExtensions/IHasTreeEntry.cs
+++ src/Nested.LinqExtensions/IHasTreeEntry.cs
@@ -1,8 +1,19 @@
 ﻿namespace Nested.LinqExtensions
 {
+    /// <summary>
+    /// Base interface for hierarchical elements.
+    /// Provides reference to TreeEntry object containing information about position in hierarchy.
+    /// </summary>
     public interface IHasTreeEntry
     {
+        /// <summary>
+        /// Gets or sets TreeEntry Id.
+        /// </summary>
         long TreeEntryId { get; set; }
+
+        /// <summary>
+        /// Gets or sets TreeEntry reference object.
+        /// </summary>
         TreeEntry TreeEntry { get; set; }
     }
 }
