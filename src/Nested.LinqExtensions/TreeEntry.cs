103f3a8d9276d8a95e7abc0f5dc62b8e0dc0cd74
benedictt <re_b_e_l@mail.ru>
2019-03-27 23:15:14 +0300

    xmldoc for TreeEntry

--- src/Nested.LinqExtensions/TreeEntry.cs
+++ src/Nested.LinqExtensions/TreeEntry.cs
@@ -2,15 +2,18 @@
 
 namespace Nested.LinqExtensions
 {
+    /// <summary>
+    /// Base class representing hierarchical data in database.
+    /// Hierarchy is defined with nested intervals encoding.
+    /// </summary>
     [DebuggerDisplay("Left: {Nv}/{Dv}; Right: {SNv}/{SDv}")]
     public class TreeEntry: IntervalQuadruple
     {
-        public TreeEntry()
-        {
-        }
-
-        public long Id { get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TreeEntry"/> class.
+        /// Using bounds given by <see cref="IIntervalQuadruple"/>.
+        /// </summary>
+        /// <param name="bounds">left/right bounds specified as (nv/dv:snv/sdv) for nested intervals encoding. </param>
         public TreeEntry(IIntervalQuadruple bounds)
         {
             this.Depth = bounds.Depth;
@@ -20,6 +23,15 @@ namespace Nested.LinqExtensions
             this.SDv = bounds.SDv;
         }
 
+        /// <summary>
+        /// Gets or Sets Tree entry unique identifier.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Set Nv/Dv, SNv/SDv and Depth values from given interval.
+        /// </summary>
+        /// <param name="source">left/right bounds specified as (nv/dv:snv/sdv) for nested intervals encoding. </param>
         public void SetFromInterval(IIntervalQuadruple source)
         {
             this.Nv = source.Nv;
