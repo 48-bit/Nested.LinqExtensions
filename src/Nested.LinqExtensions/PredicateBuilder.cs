be9a724dd742ce4ba06f3c2393895b84fa096242
benedictt <re_b_e_l@mail.ru>
2019-03-28 13:17:11 +0300

    xmldoc for predicate builder

--- src/Nested.LinqExtensions/PredicateBuilder.cs
+++ src/Nested.LinqExtensions/PredicateBuilder.cs
@@ -6,13 +6,15 @@ namespace Nested.LinqExtensions
 {
     /// <summary>
     ///     Enables the efficient, dynamic composition of query predicates.
-    ///     https://petemontgomery.wordpress.com/2011/02/10/a-universal-predicatebuilder/
+    ///     https://petemontgomery.wordpress.com/2011/02/10/a-universal-predicatebuilder/.
     /// </summary>
-    public static partial class PredicateBuilder
+    public static class PredicateBuilder
     {
         /// <summary>
         ///     Creates a predicate that evaluates to true.
         /// </summary>
+        /// <typeparam name="T">type of predicate parameter.</typeparam>
+        /// <returns>predicate that evaluates to true.</returns>
         public static Expression<Func<T, bool>> True<T>()
         {
             return param => true;
@@ -21,6 +23,8 @@ namespace Nested.LinqExtensions
         /// <summary>
         ///     Creates a predicate that evaluates to false.
         /// </summary>
+        /// <typeparam name="T">type of predicate parameter.</typeparam>
+        /// <returns>predicate that evaluates to false.</returns>
         public static Expression<Func<T, bool>> False<T>()
         {
             return param => false;
@@ -29,6 +33,9 @@ namespace Nested.LinqExtensions
         /// <summary>
         ///     Creates a predicate expression from the specified lambda expression.
         /// </summary>
+        /// <param name="predicate">source expression. </param>
+        /// <typeparam name="T">type of predicate parameter.</typeparam>
+        /// <returns>predicate expression. </returns>
         public static Expression<Func<T, bool>> Create<T>(Expression<Func<T, bool>> predicate)
         {
             return predicate;
@@ -37,7 +44,12 @@ namespace Nested.LinqExtensions
         /// <summary>
         ///     Combines the first predicate with the second using the logical "and".
         /// </summary>
-        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first,
+        /// <param name="first">first predicate. </param>
+        /// <param name="second">second predicate. </param>
+        /// <typeparam name="T">type of predicate parameter.</typeparam>
+        /// <returns>predicate combined from first and second using the logical "and". </returns>
+        public static Expression<Func<T, bool>> And<T>(
+            this Expression<Func<T, bool>> first,
             Expression<Func<T, bool>> second)
         {
             return first.Compose(second, Expression.AndAlso);
@@ -46,7 +58,12 @@ namespace Nested.LinqExtensions
         /// <summary>
         ///     Combines the first predicate with the second using the logical "or".
         /// </summary>
-        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first,
+        /// <param name="first">first predicate. </param>
+        /// <param name="second">second predicate. </param>
+        /// <typeparam name="T">type of predicate parameter.</typeparam>
+        /// <returns>predicate combined from first and second using the logical "or". </returns>
+        public static Expression<Func<T, bool>> Or<T>(
+            this Expression<Func<T, bool>> first,
             Expression<Func<T, bool>> second)
         {
             return first.Compose(second, Expression.OrElse);
@@ -55,6 +72,9 @@ namespace Nested.LinqExtensions
         /// <summary>
         ///     Negates the predicate.
         /// </summary>
+        /// <param name="expression">predicate to invert.</param>
+        /// <typeparam name="T">type of predicate parameter. </typeparam>
+        /// <returns>inverted predicate with logical "not". </returns>
         public static Expression<Func<T, bool>> Not<T>(this Expression<Func<T, bool>> expression)
         {
             var negated = Expression.Not(expression.Body);
@@ -62,14 +82,53 @@ namespace Nested.LinqExtensions
         }
 
         /// <summary>
+        /// Combine property selector expression with predicate,
+        /// passing property from selector expression as parameter.
+        /// </summary>
+        /// <param name="propertySelector">property selector expression.</param>
+        /// <param name="propertyPredicate">predicate expression taking property as parameter. </param>
+        /// <typeparam name="T1">type of property selector source. </typeparam>
+        /// <typeparam name="T2">type of predicate parameter. </typeparam>
+        /// <returns>combined predicate expression with property as parameter. </returns>
+        public static Expression<Func<T1, bool>> Compose<T1, T2>(
+            this Expression<Func<T1, T2>> propertySelector,
+            Expression<Func<T2, bool>> propertyPredicate)
+        {
+            return ParameterToMemberExpressionRebinder.CombinePropertySelectorWithPredicate(
+                propertySelector,
+                propertyPredicate);
+        }
+
+        /// <summary>
+        /// Combine property selector expression with lambda expression function,
+        /// passing property from selector expression as parameter.
+        /// </summary>
+        /// <param name="propertySelector">property selector expression.</param>
+        /// <param name="lambdaExpression">lambda expression taking property as parameter. </param>
+        /// <typeparam name="TSource">type of property selector source. </typeparam>
+        /// <typeparam name="TNavigate">type of property and lambda expression parameter. </typeparam>
+        /// <typeparam name="TResult">type lambda expression return value. </typeparam>
+        /// <returns>combined lambda expression with property as parameter. </returns>
+        public static Expression<Func<TSource, TResult>> Navigate<TSource, TNavigate, TResult>(
+            this Expression<Func<TSource, TNavigate>> propertySelector, Expression<Func<TNavigate, TResult>> lambdaExpression)
+        {
+            return ParameterToMemberExpressionRebinder.NavigatePropertySelectorsToNextLambda(propertySelector, lambdaExpression);
+        }
+
+        /// <summary>
         ///     Combines the first expression with the second using the specified merge function.
         /// </summary>
-        private static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second,
+        private static Expression<T> Compose<T>(
+            this Expression<T> first,
+            Expression<T> second,
             Func<Expression, Expression, Expression> merge)
         {
             // zip parameters (map from parameters of second to parameters of first)
             var map = first.Parameters
-                .Select((f, i) => new {f, s = second.Parameters[i]})
+                .Select((f, i) => new
+                {
+                    f, s = second.Parameters[i],
+                })
                 .ToDictionary(p => p.s, p => p.f);
 
             // replace parameters in the second lambda expression with the parameters in the first
@@ -77,19 +136,6 @@ namespace Nested.LinqExtensions
 
             // create a merged lambda expression with parameters from the first expression
             return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
-        }
-
-        public static Expression<Func<T1, bool>> Compose<T1, T2>(this Expression<Func<T1, T2>> propertySelector,
-            Expression<Func<T2, bool>> propertyPredicate)
-        {
-            return ParameterToMemberExpressionRebinder.CombinePropertySelectorWithPredicate(propertySelector,
-                propertyPredicate);
-        }
-
-        public static Expression<Func<TSource, TResult>> Navigate<TSource, TNavigate, TResult>(
-            this Expression<Func<TSource, TNavigate>> first, Expression<Func<TNavigate, TResult>> second)
-        {
-            return ParameterToMemberExpressionRebinder.NavigatePropertySelectorsToNextLambda(first, second);
         }
     }
 }
